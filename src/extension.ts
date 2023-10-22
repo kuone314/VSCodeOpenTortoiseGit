2b7cea18a0bd2ce05855cc83290f2ff3078eced5
kuone <kuone@example.com>
2023-10-22 16:33:55 +0900

    実装 一通り

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,57 @@
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
 
-export function activate(context: vscode.ExtensionContext) {
-	
-	let disposable = vscode.commands.registerCommand('registercommandline.helloWorld', () => {
-	});
 
-	context.subscriptions.push(disposable);
+///////////////////////////////////////////////////////////////////////////////////////////////////
+function decoratePath(path: String): string {
+	return '"' + path + '"';
 }
 
-export function deactivate() {}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+export const COMMAND_TYPE = {
+	log: "log",
+	settings: "settings",
+} as const;
+export type CommandType = typeof COMMAND_TYPE[keyof typeof COMMAND_TYPE];
+
+
+async function commandImpl(
+	editor: vscode.TextEditor,
+	commandType: CommandType) {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+
+	if (!workspaceFolders || workspaceFolders.length === 0) {
+		vscode.window.showErrorMessage("Workspace not found.");
+		return;
+	}
+
+	workspaceFolders.forEach(dirPath => {
+		const command = "TortoiseGitProc.exe /command:" + commandType + " /path:" + decoratePath(dirPath.uri.fsPath);
+		child_process.exec(command, (err, stdout, stderr) => {
+			if (err) {
+				vscode.window.showErrorMessage(`command: ${command} , stderr: ${stderr}`);
+			}
+		}
+		);
+	});
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('opentortoisegit.TortoiseGitLog', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) { return; }
+
+		commandImpl(editor, 'log');
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('opentortoisegit.TortoiseGitSettings', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) { return; }
+
+		commandImpl(editor, 'settings');
+	}));
+}
+
+export function deactivate() { }
+
